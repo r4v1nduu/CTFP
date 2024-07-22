7feda871974e98e47a04e44b1ebd57cf11b0003b
r4v1ndu <r4v1nduu@gmail.com>
2024-07-22 15:33:21 +0530

    port update

--- Admin-Side/app.js
+++ Admin-Side/app.js
@@ -145,7 +145,7 @@ app.get('*', (req, res) => {
     res.status(404).render('error', { message: 'Page not found', user: req.user });
 });
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
